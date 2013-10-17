af0f055d2b810010c7874e28d8da8cff98c1583d
saykat03 <saykat03@ccm-2610-d6.evergreen.edu>
2013-10-17 10:54:23 -0700

    Quiz 1

---
+++ quiz1.py
@@ -0,0 +1,11 @@
+# Graded by Whitney Barber
+# Barwhi18
+
+from lab2 import n
+# do not use single (or any) quotes around imported variables
+# do not indicated file suffix (i.e. .py, .txt. etc.)
+
+for i in range(n):
+  if i>0:
+      print "Hello World"
+# indent 'print' two spaces (or however many - just keep it consistent)
