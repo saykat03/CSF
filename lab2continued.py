b3cd5f82b7f1f7866497c66e2372c6172e3920e5
saykat03 <saykat03@ccm-2610-d6.evergreen.edu>
2013-10-24 11:34:43 -0700

    Lab 2 Updated 10/24

---
+++ lab2continued.py
@@ -0,0 +1,21 @@
+n = 43
+a, b = 0, 1
+
+series = 3
+fibonacci = 1
+
+
+if series == fibonacci:
+    for i in range(n):
+        a,b = b, a + b
+        print "Term " + str(i + 1) + ": " + str(a)
+
+if series == str(sum):
+    for i in range(n):
+        b = 1
+        print sum in range((3*b), 3, (3*n))
+
+else:
+    print "Invalid string, y'all"
+        
+
